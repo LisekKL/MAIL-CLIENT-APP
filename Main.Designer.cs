2bf8e49a90dd31dc12db3d8c0eb4f64adfec8343
LisekKL <karolina.lisowska@juniornet.onmicrosoft.com>
2017-05-30 12:37:52 +0200

    Add files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace BasicMailClient
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendMailBtn = new System.Windows.Forms.Button();
+            this.RecipientAddress = new System.Windows.Forms.TextBox();
+            this.MessageSubject = new System.Windows.Forms.TextBox();
+            this.UserMessage = new System.Windows.Forms.RichTextBox();
+            this.LABEL1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ConnectionStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SendMailBtn
+            // 
+            this.SendMailBtn.Location = new System.Drawing.Point(423, 259);
+            this.SendMailBtn.Name = "SendMailBtn";
+            this.SendMailBtn.Size = new System.Drawing.Size(274, 23);
+            this.SendMailBtn.TabIndex = 0;
+            this.SendMailBtn.Text = "Send!";
+            this.SendMailBtn.UseVisualStyleBackColor = true;
+            this.SendMailBtn.Click += new System.EventHandler(this.SendMailBtn_Click);
+            // 
+            // RecipientAddress
+            // 
+            this.RecipientAddress.Location = new System.Drawing.Point(88, 12);
+            this.RecipientAddress.Name = "RecipientAddress";
+            this.RecipientAddress.Size = new System.Drawing.Size(342, 20);
+            this.RecipientAddress.TabIndex = 1;
+            this.RecipientAddress.TextChanged += new System.EventHandler(this.RecipientAddress_TextChanged);
+            // 
+            // MessageSubject
+            // 
+            this.MessageSubject.Location = new System.Drawing.Point(88, 58);
+            this.MessageSubject.Name = "MessageSubject";
+            this.MessageSubject.Size = new System.Drawing.Size(342, 20);
+            this.MessageSubject.TabIndex = 2;
+            // 
+            // UserMessage
+            // 
+            this.UserMessage.Location = new System.Drawing.Point(88, 105);
+            this.UserMessage.Name = "UserMessage";
+            this.UserMessage.Size = new System.Drawing.Size(609, 148);
+            this.UserMessage.TabIndex = 3;
+            this.UserMessage.Text = "";
+            // 
+            // LABEL1
+            // 
+            this.LABEL1.AutoSize = true;
+            this.LABEL1.Location = new System.Drawing.Point(16, 19);
+            this.LABEL1.Name = "LABEL1";
+            this.LABEL1.Size = new System.Drawing.Size(53, 13);
+            this.LABEL1.TabIndex = 4;
+            this.LABEL1.Text = "MAIL TO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SUBJECT:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "MESSAGE:";
+            // 
+            // ConnectionStatus
+            // 
+            this.ConnectionStatus.AutoSize = true;
+            this.ConnectionStatus.Location = new System.Drawing.Point(16, 269);
+            this.ConnectionStatus.Name = "ConnectionStatus";
+            this.ConnectionStatus.Size = new System.Drawing.Size(40, 13);
+            this.ConnectionStatus.TabIndex = 7;
+            this.ConnectionStatus.Text = "Status:";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(709, 294);
+            this.Controls.Add(this.ConnectionStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LABEL1);
+            this.Controls.Add(this.UserMessage);
+            this.Controls.Add(this.MessageSubject);
+            this.Controls.Add(this.RecipientAddress);
+            this.Controls.Add(this.SendMailBtn);
+            this.Name = "MainWindow";
+            this.Text = "Basic Mail Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendMailBtn;
+        private System.Windows.Forms.TextBox RecipientAddress;
+        private System.Windows.Forms.TextBox MessageSubject;
+        private System.Windows.Forms.RichTextBox UserMessage;
+        private System.Windows.Forms.Label LABEL1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ConnectionStatus;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicMailClient
+{
+    public partial class MainWindow : Form
+    {
+        private String username, password;
+        private Socket mailServerSocket;
+
+        public string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        private bool ConnectToMailServer() {
+            try
+            {
+                this.mailServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                this.mailServerSocket.Connect("217.74.65.23", 587);
+                return true;
+            }
+            catch (SocketException exception)
+            {
+                MessageBox.Show("Connection failed!" + exception.Message);
+                return false;
+            }
+        }
+        private string ReceiveMessage()
+        {
+            byte[] serverMessage = new byte[1024];
+            if (this.mailServerSocket != null)
+            {
+                this.mailServerSocket.Receive(serverMessage);
+                string serverMessageString = ASCIIEncoding.ASCII.GetString(serverMessage);
+                return serverMessageString;
+            }
+            return "";
+        }
+
+        private void SendMessage(string message)
+        {
+            if (this.mailServerSocket != null)
+            {
+                this.mailServerSocket.Send(ASCIIEncoding.ASCII.GetBytes(message));
+            }else MessageBox.Show("Cannot send message: " + message);
+        }
+        private string convertBytesToStringArray(byte[] bytes)
+        {
+            return ASCIIEncoding.ASCII.GetString(bytes);
+        }
+
+        public void InitializeConnection()
+        {
+            if (!this.ConnectToMailServer())
+                return;
+
+            if (ReceiveMessage().Substring(0,3) == "220")
+            {
+                this.SendMessage("EHLO Zachary\r\n");
+            }
+            else return;
+       
+            if (ReceiveMessage().Substring(0, 3) == "250")
+            {
+                this.SendMessage("AUTH LOGIN\r\n");
+            }
+            else return;
+
+            if (ReceiveMessage().Substring(0, 3) == "334")
+            {
+                this.SendMessage(this.username);
+            }
+            else return;
+
+            if (ReceiveMessage().Substring(0, 3) == "334")
+            {
+                this.SendMessage(this.password);
+            }
+            else return;
+            if (ReceiveMessage().Substring(0,3) == "235")
+            {
+                this.ConnectionStatus.Text += " Connected!";
+            }
+            else return;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.username = this.Base64Encode("pas2017@interia.pl");
+            this.password = this.Base64Encode("P4SInf2017");
+            this.username += "\r\n";
+            this.password += "\r\n";
+
+            this.InitializeConnection();
+        }
+
+        private void SendMailBtn_Click(object sender, EventArgs e)
+        {
+            string EMAIL_FROM = "pas2017@interia.pl",
+            EMAIL_TO , DATA, QUIT = "QUIT\r\n", SUBJECT;
+
+            EMAIL_TO = this.RecipientAddress.Text;
+            SUBJECT = this.MessageSubject.Text;
+            DATA = this.UserMessage.Text;
+
+
+            this.SendMessage("MAIL FROM: <" + EMAIL_FROM+ ">\r\n");
+            this.SendMessage("RCPT TO: <" + EMAIL_TO + ">\r\n");
+            this.SendMessage("SUBJECT: " + SUBJECT + "\r\n\n");
+            this.SendMessage("DATA\r\n");
+            this.SendMessage(DATA + "\n.\r\n");
+
+            if (this.ReceiveMessage().Contains("250"))
+                MessageBox.Show("UDALO SIE");
+            this.SendMessage(QUIT);
+            MessageBox.Show("Mail sent!");
+        }
+
+        private void RecipientAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }     
+    }
+}
